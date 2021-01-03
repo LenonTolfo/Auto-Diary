a79c78908d15d2bf977b687ad4e069bcbaeb0ce4
Lenon <le.tolfo@yahoo.com.br>
2021-01-03 19:51:21 +0100

    finish setup bottom nav menu

--- app/src/main/java/br/com/lenon/autodiary/MainActivity.kt
+++ app/src/main/java/br/com/lenon/autodiary/MainActivity.kt
@@ -2,10 +2,39 @@ package br.com.lenon.autodiary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import br.com.lenon.autodiary.fragments.*
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+        val homeFragment = HomeFragment()
+        val carsFragment = CarsFragment()
+        val calendarFragment = CalendarFragment()
+        val historyFragment = HistoryFragment()
+        val settingsFragment = SettingsFragment()
+
+        makeCurrentFragment(homeFragment)
+
+        bottomNavigation.setOnNavigationItemSelectedListener {
+            when (it.itemId){
+                R.id.ic_home -> makeCurrentFragment(homeFragment)
+                R.id.ic_calendar -> makeCurrentFragment(calendarFragment)
+                R.id.ic_history -> makeCurrentFragment(historyFragment)
+                R.id.ic_car -> makeCurrentFragment(carsFragment)
+                R.id.ic_settings -> makeCurrentFragment(settingsFragment)
+            }
+            true
+        }
     }
+
+    private fun makeCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fl_wrapper, fragment)
+            commit()
+        }
 }
